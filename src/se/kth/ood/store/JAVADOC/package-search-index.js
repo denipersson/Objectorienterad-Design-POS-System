b367e1622e041e3d44a6193ae519aed091be1066
Deni Persson <deni.persson@gmail.com>
2023-04-05 22:45:15 +0200

    deleted unrelated files

--- src/se/kth/ood/store/JAVADOC/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"se.kth.ood.store"},{"l":"se.kth.ood.store.controller"},{"l":"se.kth.ood.store.exceptions"},{"l":"se.kth.ood.store.integration"},{"l":"se.kth.ood.store.model"},{"l":"se.kth.ood.store.startup"},{"l":"se.kth.ood.store.view"}];updateSearchResults();
